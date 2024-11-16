ea2767969c973f668667674a09f5621e56878e82
felipeverdugo <felipeverdugo016@gmail.com>
2024-11-16 13:29:21 -0300

    ejercicios-subconsultas

---
+++ querys/subs.sql
@@ -0,0 +1,11 @@
+-- ### Subconsultas
+
+-- 1. Mostrar el nombre y precio de todos los productos cuyo precio es superior al precio promedio de todos los productos.
+select p.ProductName , p.Price
+from Products p;
+
+
+-- 2. Listar los nombres de los productos que no tienen pedidos asociados.
+-- 3. Mostrar los nombres de los clientes que han realizado más de cinco pedidos.
+-- 4. Obtener el nombre y precio de los productos que tienen un precio superior al precio máximo de la categoría “Frutas y Verduras”.
+-- 5. Listar el nombre de cada cliente junto con el nombre del empleado que procesó su último pedido.
